73ee8d8bb5795d23c383f46f88362e4067364a2f
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-03-30 19:36:35 +0200

    Split CliFx metadata inspection support

--- src/InSpectra.Discovery.Tool/CliFx/CliFxMetadataInspector.cs
+++ src/InSpectra.Discovery.Tool/CliFx/CliFxMetadataInspector.cs
@@ -56,19 +56,19 @@ internal sealed class CliFxMetadataInspector
                 continue;
             }
 
-            if (!ReferencesCliFx(assembly))
+            if (!CliFxMetadataReflectionSupport.ReferencesCliFx(assembly))
             {
                 continue;
             }
 
-            foreach (var type in GetLoadableTypes(assembly))
+            foreach (var type in CliFxMetadataReflectionSupport.GetLoadableTypes(assembly))
             {
                 if (!type.IsClass || type.IsAbstract)
                 {
                     continue;
                 }
 
-                var commandAttribute = FindAttribute(type.CustomAttributes, CommandAttributeNames);
+                var commandAttribute = CliFxMetadataReflectionSupport.FindAttribute(type.CustomAttributes, CommandAttributeNames);
                 if (commandAttribute is null)
                 {
                     continue;
@@ -92,16 +92,16 @@ internal sealed class CliFxMetadataInspector
         var parameters = new List<CliFxParameterDefinition>();
         var options = new List<CliFxOptionDefinition>();
 
-        foreach (var property in GetPublicInstanceProperties(type))
+        foreach (var property in CliFxMetadataReflectionSupport.GetPublicInstanceProperties(type))
         {
-            var optionAttribute = FindAttribute(property.CustomAttributes, OptionAttributeNames);
+            var optionAttribute = CliFxMetadataReflectionSupport.FindAttribute(property.CustomAttributes, OptionAttributeNames);
             if (optionAttribute is not null)
             {
                 options.Add(CreateOptionDefinition(property, optionAttribute));
                 continue;
             }
 
-            var parameterAttribute = FindAttribute(property.CustomAttributes, ParameterAttributeNames);
+            var parameterAttribute = CliFxMetadataReflectionSupport.FindAttribute(property.CustomAttributes, ParameterAttributeNames);
             if (parameterAttribute is not null)
             {
                 parameters.Add(CreateParameterDefinition(property, parameterAttribute));
@@ -110,7 +110,7 @@ internal sealed class CliFxMetadataInspector
 
         return new CliFxCommandDefinition(
             Name: commandAttribute.ConstructorArguments.FirstOrDefault(argument => argument.ArgumentType.FullName == typeof(string).FullName).Value as string,
-            Description: GetNamedArgument<string>(commandAttribute, "Description"),
+            Description: CliFxMetadataAttributeSupport.GetNamedArgument<string>(commandAttribute, "Description"),
             Parameters: parameters.OrderBy(parameter => parameter.Order).ToArray(),
             Options: options.OrderByDescending(option => option.IsRequired).ThenBy(option => option.Name).ThenBy(option => option.ShortName).ToArray());
     }
@@ -120,12 +120,12 @@ internal sealed class CliFxMetadataInspector
         var order = attribute.ConstructorArguments.FirstOrDefault(argument => argument.ArgumentType.FullName == typeof(int).FullName).Value as int? ?? 0;
         return new CliFxParameterDefinition(
             Order: order,
-            Name: GetNamedArgument<string>(attribute, "Name") ?? property.Name.ToLowerInvariant(),
-            IsRequired: IsRequired(property),
-            IsSequence: IsSequence(property, attribute),
-            ClrType: GetClrTypeName(property.PropertyType),
-            Description: GetNamedArgument<string>(attribute, "Description"),
-            AcceptedValues: GetAcceptedValues(property.PropertyType));
+            Name: CliFxMetadataAttributeSupport.GetNamedArgument<string>(attribute, "Name") ?? property.Name.ToLowerInvariant(),
+            IsRequired: CliFxMetadataTypeSupport.IsRequired(property),
+            IsSequence: CliFxMetadataTypeSupport.IsSequence(property, attribute),
+            ClrType: CliFxMetadataTypeSupport.GetClrTypeName(property.PropertyType),
+            Description: CliFxMetadataAttributeSupport.GetNamedArgument<string>(attribute, "Description"),
+            AcceptedValues: CliFxMetadataTypeSupport.GetAcceptedValues(property.PropertyType));
     }
 
     private static CliFxOptionDefinition CreateOptionDefinition(PropertyInfo property, CustomAttributeData attribute)
@@ -134,134 +134,22 @@ internal sealed class CliFxMetadataInspector
             attribute.ConstructorArguments.FirstOrDefault(argument => argument.ArgumentType.FullName == typeof(string).FullName).Value as string);
         var shortName = attribute.ConstructorArguments.FirstOrDefault(argument => argument.ArgumentType.FullName == typeof(char).FullName).Value as char?;
         var propertyType = property.PropertyType;
-        var clrType = GetClrTypeName(propertyType);
-        var acceptedValues = GetAcceptedValues(propertyType);
+        var clrType = CliFxMetadataTypeSupport.GetClrTypeName(propertyType);
+        var acceptedValues = CliFxMetadataTypeSupport.GetAcceptedValues(propertyType);
         var nullableUnderlyingType = Nullable.GetUnderlyingType(propertyType);
         var boolType = nullableUnderlyingType ?? propertyType;
 
         return new CliFxOptionDefinition(
             Name: name,
             ShortName: shortName,
-            IsRequired: IsRequired(property),
-            IsSequence: IsSequence(property, attribute),
+            IsRequired: CliFxMetadataTypeSupport.IsRequired(property),
+            IsSequence: CliFxMetadataTypeSupport.IsSequence(property, attribute),
             IsBoolLike: string.Equals(boolType.FullName, typeof(bool).FullName, StringComparison.Ordinal),
             ClrType: clrType,
-            Description: GetNamedArgument<string>(attribute, "Description"),
-            EnvironmentVariable: GetNamedArgument<string>(attribute, "EnvironmentVariable"),
+            Description: CliFxMetadataAttributeSupport.GetNamedArgument<string>(attribute, "Description"),
+            EnvironmentVariable: CliFxMetadataAttributeSupport.GetNamedArgument<string>(attribute, "EnvironmentVariable"),
             AcceptedValues: acceptedValues,
             ValueName: property.Name);
-    }
-
-    private static IEnumerable<Type> GetLoadableTypes(Assembly assembly)
-    {
-        try
-        {
-            return assembly.GetTypes();
-        }
-        catch (ReflectionTypeLoadException ex)
-        {
-            return ex.Types.Where(type => type is not null)!;
-        }
-    }
-
-    private static IEnumerable<PropertyInfo> GetPublicInstanceProperties(Type type)
-    {
-        var chain = new Stack<Type>();
-        for (var current = type; current is not null && !string.Equals(current.FullName, typeof(object).FullName, StringComparison.Ordinal); current = current.BaseType)
-        {
-            chain.Push(current);
-        }
-
-        while (chain.Count > 0)
-        {
-            foreach (var property in chain.Pop().GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly))
-            {
-                yield return property;
-            }
-        }
-    }
-
-    private static bool ReferencesCliFx(Assembly assembly)
-        => string.Equals(assembly.GetName().Name, "CliFx", StringComparison.OrdinalIgnoreCase)
-            || assembly.GetReferencedAssemblies().Any(reference => string.Equals(reference.Name, "CliFx", StringComparison.OrdinalIgnoreCase));
-
-    private static CustomAttributeData? FindAttribute(IEnumerable<CustomAttributeData> attributes, IEnumerable<string> fullNames)
-        => attributes.FirstOrDefault(attribute => fullNames.Any(fullName =>
-            string.Equals(attribute.AttributeType.FullName, fullName, StringComparison.Ordinal)));
-
-    private static T? GetNamedArgument<T>(CustomAttributeData attribute, string name)
-    {
-        var value = attribute.NamedArguments.FirstOrDefault(argument => string.Equals(argument.MemberName, name, StringComparison.Ordinal)).TypedValue.Value;
-        return value is T typedValue ? typedValue : default;
-    }
-
-    private static bool IsRequired(PropertyInfo property)
-        => property.CustomAttributes.Any(attribute => string.Equals(attribute.AttributeType.FullName, "System.Runtime.CompilerServices.RequiredMemberAttribute", StringComparison.Ordinal));
-
-    private static bool IsSequence(PropertyInfo property, CustomAttributeData attribute)
-    {
-        var converterType = GetNamedArgument<Type>(attribute, "Converter");
-        if (converterType is not null)
-        {
-            return converterType
-                .GetInterfaces()
-                .Concat(GetBaseTypes(converterType))
-                .Any(type => string.Equals(type.FullName?.Split('`')[0], "CliFx.Activation.SequenceInputConverter", StringComparison.Ordinal));
-        }
-
-        return IsSequenceType(property.PropertyType);
-    }
-
-    private static IEnumerable<Type> GetBaseTypes(Type type)
-    {
-        for (var current = type; current is not null; current = current.BaseType)
-        {
-            yield return current;
-        }
-    }
-
-    private static bool IsSequenceType(Type type)
-    {
-        if (type.IsArray)
-        {
-            return true;
-        }
-
-        if (string.Equals(type.FullName, typeof(string).FullName, StringComparison.Ordinal))
-        {
-            return false;
-        }
-
-        return type.GetInterfaces().Any(interfaceType =>
-            string.Equals(interfaceType.FullName?.Split('`')[0], "System.Collections.Generic.IEnumerable", StringComparison.Ordinal));
-    }
-
-    private static string? GetClrTypeName(Type type)
-    {
-        if (type.IsArray)
-        {
-            return $"{GetClrTypeName(type.GetElementType()!)}[]";
-        }
-
-        if (Nullable.GetUnderlyingType(type) is { } nullableType)
-        {
-            return $"System.Nullable<{GetClrTypeName(nullableType)}>";
-        }
-
-        if (!type.IsGenericType)
-        {
-            return type.FullName ?? type.Name;
-        }
-
-        var genericName = type.GetGenericTypeDefinition().FullName?.Split('`')[0] ?? type.Name;
-        var genericArguments = string.Join(", ", type.GetGenericArguments().Select(GetClrTypeName));
-        return $"{genericName}<{genericArguments}>";
-    }
-
-    private static IReadOnlyList<string> GetAcceptedValues(Type type)
-    {
-        var enumType = Nullable.GetUnderlyingType(type) ?? type;
-        return enumType.IsEnum ? Enum.GetNames(enumType) : [];
     }
 
     private static int Score(CliFxCommandDefinition definition)
---
+++ tests/InSpectra.Discovery.Tool.Tests/CliFxMetadataTypeSupportTests.cs
@@ -0,0 +1,44 @@
+using System.Reflection;
+using Xunit;
+
+public sealed class CliFxMetadataTypeSupportTests
+{
+    [Fact]
+    public void GetClrTypeName_Formats_Nullable_And_Generic_Types()
+    {
+        var clrTypeName = CliFxMetadataTypeSupport.GetClrTypeName(typeof(Dictionary<string, int?>));
+
+        Assert.Equal("System.Collections.Generic.Dictionary<System.String, System.Nullable<System.Int32>>", clrTypeName);
+    }
+
+    [Fact]
+    public void GetAcceptedValues_Returns_Enum_Names_For_Nullable_Enums()
+    {
+        var acceptedValues = CliFxMetadataTypeSupport.GetAcceptedValues(typeof(SampleMode?));
+
+        Assert.Equal(["Basic", "Advanced"], acceptedValues);
+    }
+
+    [Fact]
+    public void IsSequence_Returns_True_For_Array_Properties_Without_Converter()
+    {
+        var property = typeof(SampleCommand).GetProperty(nameof(SampleCommand.Paths), BindingFlags.Instance | BindingFlags.Public)!;
+        var attribute = property.CustomAttributes.First();
+
+        var isSequence = CliFxMetadataTypeSupport.IsSequence(property, attribute);
+
+        Assert.True(isSequence);
+    }
+
+    private enum SampleMode
+    {
+        Basic,
+        Advanced,
+    }
+
+    private sealed class SampleCommand
+    {
+        [System.ComponentModel.Description("sample")]
+        public string[] Paths { get; init; } = [];
+    }
+}
